fix negative action variance prob

CartPole.py
@@ -8,11 +8,11 @@ import pandas as pd
 import matplotlib.pyplot as plt 
 from gym.spaces import Discrete, Box
 
-np.random.seed(0)
+# np.random.seed(0)
 
 env = gym.make('CartPole-v1')
 
-def rollout(policy, timesteps, discrete_ac):
+def rollout(policy, timesteps, discrete_ac=False):
 	"""
 	get training data for GP to model the transition function
 	"""
@@ -63,14 +63,16 @@ elif isinstance(env.action_space, Box):
 	discrete_ac = False
 
 # TODO
-X, Y, total_step, reward = rollout(random_policy, 20, discrete_ac)
+X, Y, total_step, reward = rollout(random_policy, 40, discrete_ac)
 
 for i in range(1, 3):
-	X_, Y_, step, reward = rollout(random_policy, 20, discrete_ac)
+	X_, Y_, step, reward = rollout(random_policy, 40, discrete_ac)
 	total_step += step
 	X = np.vstack((X, X_))
 	Y = np.vstack((Y, Y_))
 
+# print("X: ", X)
+# print("Y: ", Y)
 
 # hyperparams
 RENDER = False
@@ -80,47 +82,61 @@ max_episode = 2
 # max_episode_step = 3000
 
 controller = Actor(action_dim=action_dim, action_choice=action_choice, state_dim=state_dim, learning_rate=learning_rate, discrete_ac=discrete_ac)
-cartpole_reward = CartPoleReward()  # consider to reset Reward function
-pilco = PILCO(X, Y, controller=controller, reward=cartpole_reward)
+# # load actor
+# try:
+# 	controller.load_weights('./checkpoints/actor.ckpt')
+# except:
+# 	print("do not find checkpoint.")
+# add how many optim steps for this actor model
+
+cartpole_reward = CartPoleReward()
+
+# load gp
+try:
+	pilco = load_pilco("./checkpoints/gp/", controller, cartpole_reward)
+except:
+	print("can not find saved gp models")
+	pilco = PILCO(X, Y, controller=controller, reward=cartpole_reward)
 
 reward_list = []
 total_episode = 3
 ep_step_list = []
+X_init = X[:, 0: state_dim]
 
 for rollouts in range(max_episode):
 	print("***" * 30)
 	print("the " + str(rollouts) + "th rollout begins.")
+	print("num optim: ", pilco.controller.get_num_optim())
 	print("***" * 30)
 	# optimize GP
 	pilco.optimize_gp()
+
 	# the controller optimization
-	states = X[:, 0: state_dim]
-	print(states.shape[0])
-	pilco.optimize_controller(states, 15)
+	# states = X[:, 0: state_dim]
+	# print(states.shape[0])
+	pilco.optimize_controller(X_init, 15, num_optim=20, gamma=reward_decay)
 
 	# save controller's weights
 	print("saving the controller.")
 	pilco.controller.save_weights('./checkpoints/actor.ckpt')
 
-	# Here we use learned controller to sample data in env for GP optim, we can get the reward at the same time
-	# Q: should we use num of episode or num of step in env ?
-
 	X_new, Y_new, step, reward = rollout(pilco_policy, 100, discrete_ac)
 	# update dataset, why update instead of replace
-	# X = np.vstack((X, X_new))
-	# Y = np.vstack((Y, Y_new))
-	X, Y = np.array(X_new), np.array(Y_new)
+	X = np.vstack((X, X_new))
+	Y = np.vstack((Y, Y_new))
+	pilco.mgpr.set_XY(X, Y)
+	X_init = np.array(X_new)[:, 0: state_dim]
 
 	total_step += step
 	ep_step_list.append(total_step)
 	reward_list.append(reward)
-	total_step += 1
+	total_episode += 1
 	# for i in range(1, 3):
 	# 	X_, Y_, step, reward = rollout(pilco_policy, 100, discrete_ac)
 	# 	X = np.vstack((X, X_))
 	# 	Y = np.vstack((Y, Y_))
 
-	pilco.mgpr.set_XY(X, Y)
+
 
 # save controller's weights
 pilco.controller.save_weights('./checkpoints/actor.ckpt')

actor.py
@@ -26,6 +26,9 @@ class Actor():
 		self.weight3 = tf.Variable(tf.random_normal([self.hidden_size, self.action_dim], dtype=tf.float64), name="fc3_weight")
 		self.bias3 = tf.Variable(tf.random_normal([self.action_dim], dtype=tf.float64), name="fc3_bias")
 
+		# num of optimizations already done
+		self.num_optim = tf.Variable(0)
+
 		self._build_net()
 
 		self.sess = tf.Session()
@@ -147,6 +150,9 @@ class Actor():
 		s_ac = np.squeeze(s_ac, 0)	  # [action_dim]
 
 		print("s action", s_ac)
+
+		# abs, s_ac should not be negative
+		s_ac = abs(s_ac)
 
 		# add noise to solve Cholesky decomposition prob
 		# batched_eye = np.eye(s_ac.shape[0])
@@ -271,8 +277,12 @@ class Actor():
 		})
 
 		print("Controller optimization finished.")
+		self.num_optim = tf.add(self.num_optim, 1)
 		# reset the episode record
 		self.ep_m_obs, self.ep_s_obs, self.ep_pilco_r, self.ep_ac_choosen = [], [], [], []
+
+	def get_num_optim(self):
+		return self.sess.run(self.num_optim)
 
 	def store_transition(self, m_obs, s_obs, pilco_return, action_choosen=None):
 		"""
@@ -285,12 +295,12 @@ class Actor():
 		self.ep_pilco_r = pilco_return
 
 	def save_weights(self, path):
-		saver = tf.train.Saver([self.weight1, self.weight2, self.weight3, self.bias1, self.bias2, self.bias3])
+		saver = tf.train.Saver([self.weight1, self.weight2, self.weight3, self.bias1, self.bias2, self.bias3, self.num_optim])
 		save_path = saver.save(self.sess, path)
 		print("model saved in file: %s" % save_path)
 
 	def load_weights(self, path):
-		saver = tf.train.Saver([self.weight1, self.weight2, self.weight3, self.bias1, self.bias2, self.bias3])
+		saver = tf.train.Saver([self.weight1, self.weight2, self.weight3, self.bias1, self.bias2, self.bias3, self.num_optim])
 		saver.restore(self.sess, path)
 
 

pilco.py
@@ -57,7 +57,7 @@ class PILCO:
         reward = self.predict(m_x, s_x, horizon)[2]
         return reward
 
-    def optimize_controller(self, states, horizon, num_optim=8):
+    def optimize_controller(self, states, horizon, num_optim=8, gamma=1.):
         """
         optimize controller's parameters
         :param: states: a array of init states
@@ -72,7 +72,7 @@ class PILCO:
             print("Evaluate the " + str(i) + "th init state, total init states: " + str(len(states[:num_optim])))
             # self.predict(state, np.diag(np.ones(self.state_dim) * 0.1), horizon)
             m_x = np.expand_dims(x, 0)
-            self.get_data(m_x, s_x, horizon)
+            self.get_data(m_x, s_x, horizon, gamma=gamma)
             self.controller.optimize()
         end = time.time()
         print("Finished with " + str(len(states[:num_optim])) + " times policy/controller optimizations in %.1f seconds" % (end - start))
@@ -201,8 +201,8 @@ class PILCO:
             ep_ac.append(ac)
             m_x, s_x = self.propagate(m_x, s_x, m_u, s_u, c_xu)
             # solve the "nan in array" prob
-            m_x[np.isnan(m_x)] = 0.
-            s_x[np.isnan(s_x)] = 0.
+            # m_x[np.isnan(m_x)] = 0.01
+            # s_x[np.isnan(s_x)] = ?
             # we need to change here if reward depends on action
             current_reward, done = self.reward.compute_gaussian_reward(np.squeeze(m_x, 0), s_x)
             print("m_state: ", m_x, "s_state: ", s_x)

utils.py
@@ -11,23 +11,23 @@ def save_pilco(path, X, Y, pilco, sparse=False):
         with open(path+ 'n_ind.txt', 'w') as f:
             f.write('%d' % pilco.mgpr.num_induced_points)
             f.close()
-    np.save(path + 'pilco_values.npy', pilco.read_values())
+    # np.save(path + 'pilco_values.npy', pilco.read_values())
     for i, m in enumerate(pilco.mgpr.models):
         np.save(path + "model_" + str(i) + ".npy", m.read_values())
 
-def load_pilco(path, sparse=False):
+def load_pilco(path, controller=None, reward=None, sparse=False):
     X = np.loadtxt(path + 'X.csv', delimiter=',')
     Y = np.loadtxt(path + 'Y.csv', delimiter=',')
     if not sparse:
-        pilco = PILCO(X, Y)
+        pilco = PILCO(X, Y, controller=controller, reward=reward)
     else:
         with open(path+ 'n_ind.txt', 'r') as f:
             n_ind = int(f.readline())
             f.close()
         pilco = PILCO(X, Y, num_induced_points=n_ind)
-    params = np.load(path + "pilco_values.npy").item()
-    pilco.assign(params)
-    for i,m in enumerate(pilco.mgpr.models):
+    # params = np.load(path + "pilco_values.npy").item()
+    # pilco.assign(params)
+    for i, m in enumerate(pilco.mgpr.models):
         values = np.load(path + "model_" + str(i) + ".npy").item()
         m.assign(values)
     return pilco